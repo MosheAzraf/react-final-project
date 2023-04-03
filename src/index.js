cfe76534ce22565625c390fbd4f724f8aba5f1d7
Moshe Azraf <moshiko458@gmail.com>
2023-04-03 20:31:23 +0300

    added support for routing

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import { legacy_createStore as createStore } from 'redux';
 import { Provider } from 'react-redux';
 import storeReducers from './redux';
+import { BrowserRouter } from 'react-router-dom';
 
 const store = createStore(storeReducers);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </BrowserRouter>
 );
 
 
