28587b36813a4b74490ad36294363ffba6d55b37
Moshe Azraf <moshiko458@gmail.com>
2023-04-06 18:19:50 +0300

    fixed layout from productslist component to products page

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,24 +1,10 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 
 const ProductsList = () => {
-    const products = useSelector(state => state.products);
-    console.log(products);
-
-    const filterProductsByCustomers = () => {
-      
-    }
 
   return (
     <div className="container">
-      <div>total amount of purchased products: {}</div>
-      <div className=' grid grid-rows-2  border border-solid'>
-        <div className=' grid grid-flow-col border border-solid'>
-          <div className=' col-span-1 border border-solid'>products</div>
-          <div className=' col-span-1 border border-solid'>customers</div>
-        </div>
-        <div>combobox</div>
-      </div>
+
     </div>
   )
 }
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import ProductsList from '../components/ProductsList'
+import ProductsList from '../components/ProductsList';
 
 const Products = () => {
   return (
-    <div className='container'>Products
-    <ProductsList/>
+    <div className="container">
+      <div>total amount of purchased products: {}</div>
+      <div className=' grid grid-rows-2  border border-solid'>
+        <div className=' grid grid-flow-col border border-solid'>
+          <div className=' col-span-1 border border-solid'>products</div>
+          <div className=' col-span-1 border border-solid'>customers</div>
+        </div>
+        <div>combobox</div>
+      </div>
     </div>
   )
 }
