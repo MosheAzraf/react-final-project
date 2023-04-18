76d3857b94f4d4d027ee9d52a8186252b5c965da
Moshe Azraf <moshiko458@gmail.com>
2023-04-18 18:08:32 +0300

    created sorted data based and customer, thier purchases and products

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import { useSelector } from 'react-redux';
 
 const Customers = () => {
+  const customers = useSelector(state => state.customers.customers)
+  const products = useSelector(state => state.products.products);
+  const purchases = useSelector(state => state.purchases.purchases);
+  const [data, setData] = useState([]);
+  //console.log("customers:", customers, "products:", products, "purchases:", purchases);
+
+  useEffect(()=> {
+   const combinedData = customers.map((customer)=> {
+    const customerPurchases = purchases.filter((purchase)=> purchase.customerId === customer.id);
+    const customerProducts = customerPurchases.map((purchase)=> {
+      const productInfo = products.filter((product)=> product.id === purchase.productId);
+      return {...productInfo}
+    } )
+    return {customer, customerPurchases,customerProducts }
+   })
+   setData(combinedData);
+   console.log(combinedData)
+  },[])
+
+
+
   return (
     <div className='container'>Customers</div>
   )
